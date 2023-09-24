2c269a2da8f2b7384645bfeb5a57084fc27c8142
webagent-dev <webagent24@gmail.com>
2023-09-24 13:23:59 +0100

    weel understand and work with

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,26 +1,27 @@
-
 pragma solidity ^0.5.0;
-
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
- contract Token {
-  using SafeMath for uint;
-   // variables in smart contract
-    string  public name = 'Dapp Token';
+
+contract Token{
+    using SafeMath for uint;
+
+    string public name = 'Dapp Token';
     string public symbol = 'Dapp';
-    uint256 public decimals = 18;
+    uint256 public decimal = 18;
     uint256 public totalSupply;
 
-//mapping 
-mapping(address => uint256) public balanceOf;
 
-// contructors
-    constructor() public {     
-       totalSupply = 1000000 * (10 ** decimals);
-       balanceOf[msg.sender] = totalSupply;
-    } 
-    // transfer function
-    function transfer(address _to, uint256 _value) public returns (bool sucess){
-      balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-      balanceOf[_to] = balanceOf[_to].add(_value);
+mapping(address => uint256 ) public balanceOf;
+    constructor () public {
+        totalSupply = 1000000 * (10 ** decimal);
+        balanceOf[msg.sender] = totalSupply;
     }
- }
+
+// track balance 
+// send Token
+function transfer (address _to, uint256 _value) public returns (bool success){
+    balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+    balanceOf[_to] = balanceOf[_to].add(_value);
+    return true;
+}
+
+}
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,60 +1,54 @@
 const Token = artifacts.require('./Token')
+import { tokens } from '../helpers/helper'
 require('chai')
 .use(require('chai-as-promised'))
 .should()
-contract('Token', ([deployer, receiver]) => {
-    let token;
-    const name = 'Dapp Token'
-    const symbol = 'Dapp'
-    const decimals = '18' 
-    const totalsupply =  '1000000000000000000000000'
-    beforeEach( async() => {
-        token = await Token.new()
-    })
-    // variable test
-    describe("development", () => { 
-        // check for name
-        it("track the name", async () => {
-           const result = await token.name()
-           result.should.equal(name)
-        })
-        // check for symbol
-        it('tracks the symbol', async() => {
-            const result = await token.symbol()
-            result.should.equal(symbol)
-        })
-        // check for decimals
-         it('tracks the decimals', async() => {
-            const result = await token.decimals()
-            result.toString().should.equal(decimals)
-        })
-        // check for total supply
-         it('tracks the total supply', async() => {
-            const result = await token.totalSupply()
-            result.toString().should.equal(totalsupply)
-        })
-        it('it assign total supply to deployer', async () => {
-            const result = await token.balanceOf(deployer)
-            result.toString().should.equal(totalsupply)
-        })
-    })
-    //transfer of token tested
-     describe('transfer of token', () => {
-          it('it show token balance', async () => {
-            let balanceOf;
-            // before transfer
-            balanceOf = await token.balanceOf(deployer)
-            console.log('deployer before balence', balanceOf.toString())
-            balanceOf = await token.balanceOf(receiver)
-            console.log('receiver before balence', balanceOf.toString())
-            // transfer
-            await token.transfer(receiver, '100000000000000000000',{from: deployer})
-            // after transfer
-             balanceOf = await token.balanceOf(deployer)
-            console.log('deployer after balence', balanceOf.toString())
-            balanceOf = await token.balanceOf(receiver)
-            console.log('receiver after balence', balanceOf.toString())
-        })
-      })
+contract("Token", ([deployer, receiver]) => {
 
+    let token;
+    const name = 'Dapp Token';
+    const sym = 'Dapp'
+    const dec = '18';
+    const supply =tokens(1000000).toString();
+
+    describe("development", () => {
+        it('checks var name', async () => {
+            token = await Token.new();
+            const result = await token.name();
+            result.should.equal(name);
+        })
+          it('checks var symbol', async () => {
+            token = await Token.new();
+            const result = await token.symbol();
+            result.should.equal(sym);
+        }) 
+         it('checks var decimal', async () => {
+            token = await Token.new();
+            const result = await token.decimal();
+            result.toString().should.equal(dec);
+        })
+          it('checks var totalSupply', async () => {
+            token = await Token.new();
+            const result = await token.totalSupply();
+            result.toString().should.equal(supply);
+        }) 
+         it('checks var balanceOf', async () => {
+            token = await Token.new();
+            const result = await token.balanceOf(deployer);
+            result.toString().should.equal(supply);
+        })
+         
+    })
+    //transfer token test
+  describe("transfer Token", () => {
+    it('it check transfered token', async () => {
+        let balanceOf;
+        const token = await Token.new();
+        await token.transfer(receiver, tokens(100), {from: deployer})
+        balanceOf = await token.balanceOf(deployer)
+        balanceOf.toString().should.equal(tokens(999900).toString())
+        balanceOf = await token.balanceOf(receiver)
+        balanceOf.toString().should.equal(tokens(100).toString())
+    })
+  })
 })
