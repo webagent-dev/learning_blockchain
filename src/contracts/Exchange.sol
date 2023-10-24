7a24ec8b99039795b6fbf7a63c587a82e446cf18
webagent-dev <webagent24@gmail.com>
2023-10-24 16:22:57 +0100

    complete exchange test

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -17,16 +17,32 @@
 // [ ] Charge fees
 
 pragma solidity ^0.5.0;
-
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import './Token.sol';
 contract Exchange {
-
-    // state Variable
-     address public feeAccount; // the account that receives exchange
-     uint256 public feePercent; // the fee percentage
-
-     // constructor
-     constructor (address _feeAccount, uint256 _feePercent) public {
+   using SafeMath for uint;
+  // state variable
+  address public feeAccount;
+  uint256 public feePercent;
+  address constant ETHER = address(0);
+// mapping
+mapping(address => mapping(address => uint256)) public tokens;
+//events
+event Deposit (address token, address user, uint256 amount, uint256 balance);
+  // constructor
+  constructor (address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount;
         feePercent = _feePercent;
-     }
+  }
+  // functions
+  function depositEther () payable public {
+    tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
+     emit Deposit(ETHER, msg.sender, msg.value, tokens[ETHER][msg.sender]);
+  }
+  function depositToken (address _token, uint256 _amount) public {
+      require(_token != ETHER);
+      require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+      tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
+      emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
+  }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,25 +1,65 @@
 const Exchange = artifacts.require('./Exchange')
-const { tokens, rejectedError } = require('../helpers/helper')
+const Token = artifacts.require('./Token')
 require('chai')
 .use(require('chai-as-promised'))
 .should()
+const { tokens, rejectedError, ETHER_ADDRESS_ZERO } = require('../helpers/helper')
 
 
-contract("Exchange", ([deployer, feeAccount]) => {
-        let exchange;
-        const feePercent = 10;
-        beforeEach(async() => {
-            exchange = await Exchange.new(feeAccount, feePercent)
-        })
-        
-        describe('exchange development', () => {
-            it('checks fee account', async () => {
-                const result = await exchange.feeAccount()
+contract('Exchange', ([deployer, feeAccount, user1]) => {
+      let exchange, result, token;
+        const feePercent = 10
+            beforeEach(async () => {
+                token = await Token.new()
+                token.transfer(user1, tokens(100), {from: deployer})
+                exchange = await Exchange.new(feeAccount, feePercent)
+            })
+    describe('development', () => {
+            it('check feeAccount', async() => {
+                result = await exchange.feeAccount()
                 result.should.equal(feeAccount)
             })
-             it('checks fee percent', async () => {
-                const result = await exchange.feePercent()
+            it('check feePercent', async() => {
+                result = await exchange.feePercent()
                 result.toString().should.equal(feePercent.toString())
             })
+    }) 
+    describe('deposit ETHER', () => {
+        
+    })
+    describe('deposit token', () => {
+        describe('success deposit', () => {
+               beforeEach(async () => {
+                await token.approve(exchange.address, amount, {from: user1})
+                 result = await exchange.depositToken(token.address, amount, {from: user1})
+            })
+              const amount = tokens(10)
+                  let balanceOf;
+                it('check token deposit amount', async () => {
+                        balanceOf = await token.balanceOf(exchange.address)
+                        balanceOf.toString().should.equal(amount.toString())
+                     balanceOf = await exchange.tokens(token.address, user1)
+                     balanceOf.toString().should.equal(amount.toString())
+                })
+                it('it emit deposit event', async () => {
+                    const log = result.logs[0]
+                 log.event.should.equal('Deposit')
+                 const event = log.args
+
+                 event.token.should.equal(token.address, ' token address is correct')
+                  event.user.should.equal(user1, ' user  is correct')
+                   event.amount.toString().should.equal(tokens(10).toString(), ' amount is correct')
+                    event.balance.toString().should.equal(tokens(10).toString(), ' balance  is correct')
+                })
+
         })
+        describe('failure deposit', () => {
+            it('reject ether deposit', async () => {
+                await exchange.depositToken(ETHER_ADDRESS_ZERO, tokens(10), {from: user1}).should.be.rejectedWith(rejectedError)
+            })
+            it('check if no token approved', async () => {
+                await exchange.depositToken(token.address, tokens(10), { from: user1}).should.be.rejectedWith(rejectedError)
+            })
+        })
+    })
 })
