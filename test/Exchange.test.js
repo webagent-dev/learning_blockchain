19a2a05b0aba20dc3548e14fef1b742adc73f970
webagent-dev <webagent24@gmail.com>
2023-11-08 15:13:02 +0100

    deposit ether pass

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -29,6 +29,27 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
         
     })
     describe('deposit ether', () => {
-
+       describe('success deposit', () => {
+        amount = ether(1)
+        beforeEach(async () => {
+            result = await exchange.depositEther({from: user1, value: amount})
+        })
+        it('check deposit token', async() => {
+            const balance = await exchange.tokens(ETHER_ADDRESS, user1)
+            balance.toString().should.equal(amount.toString())
+        })
+        it('emit ether deposit event', async() => {
+             const log = result.logs[0]
+             log.event.should.equal("Deposit")
+             const event = log.args
+             event.token.should.equal(ETHER_ADDRESS, 'token is correct')
+             event.user.should.equal(user1, 'user is correct')
+             event.amount.toString().should.equal(amount.toString(), 'amount is correct')
+             event.balance.toString().should.equal(amount.toString(), 'balance is correct')
+        })
+    })
+    describe('failure deposit', () => {
+        
+    })
     })
 })
