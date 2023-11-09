8689edb1025fb53bed7c02c67c07537604042da8
webagent-dev <webagent24@gmail.com>
2023-11-09 03:02:27 +0100

    token deposit pass with error with ether deposit

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -9,7 +9,9 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
     let token, exchange, amount, result;
     const feePercent = 10;
     beforeEach(async() => {
+        amount = tokens(100)
         token = await Token.new();
+        await token.transfer(user1, amount, {from: deployer})
         exchange = await Exchange.new(feeAccount, feePercent);
     })
     describe('development', () => {
@@ -24,7 +26,7 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
     })
     describe('fallback check', () => {
         it('revert unknown transaction', async() => {
-            await exchange.sendTransaction({from: user1, value: ether(1)}).should.be.rejectedWith(rejectedError)
+            await exchange.sendTransaction({value: 1, from: user1}).should.be.rejectedWith(rejectedError)
         })
         
     })
@@ -51,5 +53,35 @@ contract("Exchange", ([deployer, feeAccount, user1]) => {
     describe('failure deposit', () => {
         
     })
+
+    })
+    describe('deposit token', () => {
+        describe('success deposit', () => {
+            beforeEach(async() => {
+                amount = tokens(10)
+                await token.approve(exchange.address, amount, {from: user1})
+                result = await exchange.depositToken(token.address, amount, {from: user1})
+            })
+            it('check token balance', async () => {
+                const balance = await token.balanceOf(exchange.address)
+                balance.toString().should.equal(amount.toString())
+            })
+            it('track token balance', async () => {
+                const balance = await exchange.tokens(token.address, user1)
+                balance.toString().should.equal(amount.toString())
+            })
+            it('emit token transfer event', async () => {
+                const log = result.logs[0]
+                log.event.should.equal("Deposit")
+                const event = log.args
+                event.token.should.equal(token.address, 'token is correct')
+                event.user.should.equal(user1, 'user is correct')
+                event.amount.toString().should.equal(amount.toString(), 'amount is correct')
+                event.balance.toString().should.equal(amount.toString(), 'balance is correct')
+            })
+        })
+        describe('failure deposit', () => {
+
+        })
     })
 })
